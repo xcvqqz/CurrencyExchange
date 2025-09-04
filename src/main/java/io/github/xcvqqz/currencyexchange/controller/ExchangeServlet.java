0eb900ac1d184f9725ea2ce0da519484d4451c85
Максим <Grossholder77@gmail.com>
2025-09-04 23:34:42 +0300

    ref: удаление класса ModelMapper и полная замена ручной маппинг.

--- src/main/java/io/github/xcvqqz/currencyexchange/controller/ExchangeServlet.java
+++ src/main/java/io/github/xcvqqz/currencyexchange/controller/ExchangeServlet.java
@@ -24,18 +24,8 @@ public class ExchangeServlet extends BasicServlet {
         String amountParam = request.getParameter("amount");
         BigDecimal amount = new BigDecimal(amountParam);
 
-        ExchangeResponseDto exchangeResponseDto = exchangeService.convert(from, to,amount);
+        ExchangeResponseDto exchangeResponseDto = exchangeService.convert(from, to, amount);
         doResponse(response, SC_OK, exchangeResponseDto);
 
     }
-
-
-
-
-//    GET /exchange?from=BASE_CURRENCY_CODE&to=TARGET_CURRENCY_CODE&amount=$AMOUNT #
-
-//    Расчёт перевода определённого количества средств из одной валюты в другую.
-//    Пример запроса -
-//    GET /exchange?from=USD&to=AUD&amount=10.
-
 }
