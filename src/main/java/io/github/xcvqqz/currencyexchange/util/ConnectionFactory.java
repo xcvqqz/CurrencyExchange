11be6d25e54523b7238a128fbb4b0c300d6f57fe
Максим <Grossholder77@gmail.com>
2025-09-08 14:46:34 +0300

    ref: добавление в pom.xml зависимость HikariCP, а также реализация пула соединений для JDBC с помощью HikariCP

--- src/main/java/io/github/xcvqqz/currencyexchange/util/ConnectionFactory.java
+++ src/main/java/io/github/xcvqqz/currencyexchange/util/ConnectionFactory.java
@@ -1,23 +1,51 @@
 package io.github.xcvqqz.currencyexchange.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String DB_URL = "jdbc:sqlite:C:\\Users\\Максим\\Desktop\\Java\\3 проект\\CurrencyExchange\\DataBase\\currency_exchange.db";
+    //private static final String DB_URL = "jdbc:sqlite:C:\\Users\\Максим\\Desktop\\Java\\3 проект\\CurrencyExchange\\DataBase\\currency_exchange.db";
+
+
+    private static final HikariDataSource dataSource;
+    private static final HikariConfig config;
 
     static {
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("JDBC Driver not found", e);
-        }
+
+        config = new HikariConfig();
+
+        String dataBasePath = ConnectionFactory.class.getClassLoader().getResource("").getPath();
+        config.setJdbcUrl("jdbc:sqlite:" + dataBasePath);
+        config.setDriverClassName("org.sqlite.JDBC");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30000); // 30 секунд
+        config.setMaxLifetime(1800000); // 30 минут
+        config.setPoolName("CurrencyExchangePool");
+
+        dataSource = new HikariDataSource(config);
+
     }
 
     public static Connection getConnection() throws SQLException {
-            Connection connection = DriverManager.getConnection(DB_URL);
-            return connection;
+        return dataSource.getConnection();
+    }
+
+    public static DataSource getDataSource(){
+        return dataSource;
+    }
+
+    public static void closeDataSource() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
         }
+    }
+
+
 }
