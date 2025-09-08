41edaff98fc49ab72df29b83bbef507b4945d820
Максим <Grossholder77@gmail.com>
2025-09-08 22:07:02 +0300

    ref: 1) добавление в resources директорию database и добавление в данную директорию configurations.properties и currency_exchange.db. 2) рефакторинг ConnectionFactory: считывание configurations.properties для работы с БД.

--- src/main/java/io/github/xcvqqz/currencyexchange/dto/ExchangeResponseDto.java
+++ src/main/java/io/github/xcvqqz/currencyexchange/dto/ExchangeResponseDto.java
@@ -3,7 +3,6 @@ package io.github.xcvqqz.currencyexchange.dto;
 import java.math.BigDecimal;
 import io.github.xcvqqz.currencyexchange.entity.Currency;
 
-
 public record ExchangeResponseDto(
         Currency baseCurrency,
         Currency targetCurrency,
--- src/main/java/io/github/xcvqqz/currencyexchange/util/ConnectionFactory.java
+++ src/main/java/io/github/xcvqqz/currencyexchange/util/ConnectionFactory.java
@@ -2,50 +2,35 @@ package io.github.xcvqqz.currencyexchange.util;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import io.github.xcvqqz.currencyexchange.exception.DataBaseException;
 
-import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
-    //private static final String DB_URL = "jdbc:sqlite:C:\\Users\\Максим\\Desktop\\Java\\3 проект\\CurrencyExchange\\DataBase\\currency_exchange.db";
-
-
+    private static final Properties properties;
     private static final HikariDataSource dataSource;
     private static final HikariConfig config;
+    private static final String DB_PATH_CONFIGURATION = "database/configurations.properties";
+    private static final String DB_ERROR = "Database error: failed to establish connection to the database";
 
     static {
+        try {
+            properties = new Properties();
+            properties.load(ConnectionFactory.class.getClassLoader()
+                    .getResourceAsStream(DB_PATH_CONFIGURATION));
 
-        config = new HikariConfig();
-
-        String dataBasePath = ConnectionFactory.class.getClassLoader().getResource("").getPath();
-        config.setJdbcUrl("jdbc:sqlite:" + dataBasePath);
-        config.setDriverClassName("org.sqlite.JDBC");
-        config.setMaximumPoolSize(10);
-        config.setMinimumIdle(2);
-        config.setConnectionTimeout(30000); // 30 секунд
-        config.setMaxLifetime(1800000); // 30 минут
-        config.setPoolName("CurrencyExchangePool");
-
-        dataSource = new HikariDataSource(config);
-
+            config = new HikariConfig(properties);
+            dataSource = new HikariDataSource(config);
+        } catch (IOException e) {
+            throw new DataBaseException(DB_ERROR);
+        }
     }
 
     public static Connection getConnection() throws SQLException {
         return dataSource.getConnection();
     }
-
-    public static DataSource getDataSource(){
-        return dataSource;
-    }
-
-    public static void closeDataSource() {
-        if (dataSource != null && !dataSource.isClosed()) {
-            dataSource.close();
-        }
-    }
-
-
 }
